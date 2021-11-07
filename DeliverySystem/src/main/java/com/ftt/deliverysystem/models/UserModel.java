6c9cb56604f551d420f4501a99590b401f3101d1
cdArth <arthurjob@gmail.com>
2021-11-07 17:55:00 -0300

    Signed-off-by: cdArth <arthurjob@gmail.com>

--- DeliverySystem/src/main/java/com/ftt/deliverysystem/models/UserModel.java
+++ DeliverySystem/src/main/java/com/ftt/deliverysystem/models/UserModel.java
@@ -8,36 +8,37 @@ import java.sql.Date;
  */
 public class UserModel {
 //USUARIO (id_usuario, usuario, senha, email, telefone, privilegio_administrador)
-private int idUser;
-private String User;
-private String senha;
-private String email;
-private String telefone;
-private boolean premium;
 
-    public UserModel(int idUser, String User, String senha, String email, String telefone, boolean premium) {
-        this.idUser = idUser;
-        this.User = User;
+    private int id_usuario;
+    private String usuario;
+    private String senha;
+    private String email;
+    private String telefone;
+    private java.sql.Date data_nascimento;
+    
+    public UserModel(int id_usuario, String usuario, String senha, String email, String telefone, Date data_nascimento) {
+        this.id_usuario = id_usuario;
+        this.usuario = usuario;
         this.senha = senha;
         this.email = email;
         this.telefone = telefone;
-        this.premium = premium;
+        this.data_nascimento = data_nascimento;
     }
 
-    public int getIdUser() {
-        return idUser;
+    public int getId_usuario() {
+        return id_usuario;
     }
 
-    public void setIdUser(int idUser) {
-        this.idUser = idUser;
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
     }
 
-    public String getUser() {
-        return User;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setUser(String User) {
-        this.User = User;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
     public String getSenha() {
@@ -64,76 +65,12 @@ private boolean premium;
         this.telefone = telefone;
     }
 
-    public boolean isPremium() {
-        return premium;
-    }
-
-    public void setPremium(boolean premium) {
-        this.premium = premium;
-    }
-
-    private int _id_usuario;
-    private String _usuario;
-    private String _senha;
-    private String _email;
-    private String _telefone;
-    private java.sql.Date _data_nascimento;
-
-    public UserModel(int _id_usuario, String _usuario, String _senha, String _email, String _telefone, Date _data_nascimento) {
-        this._id_usuario = _id_usuario;
-        this._usuario = _usuario;
-        this._senha = _senha;
-        this._email = _email;
-        this._telefone = _telefone;
-        this._data_nascimento = _data_nascimento;
-    }
-
-    public int getId_usuario() {
-        return _id_usuario;
-    }
-
-    public void setId_usuario(int _id_usuario) {
-        this._id_usuario = _id_usuario;
-    }
-
-    public String getUsuario() {
-        return _usuario;
-    }
-
-    public void setUsuario(String _usuario) {
-        this._usuario = _usuario;
-    }
-
-    public String getSenha() {
-        return _senha;
-    }
-
-    public void setSenha(String _senha) {
-        this._senha = _senha;
-    }
-
-    public String getEmail() {
-        return _email;
-    }
-
-    public void setEmail(String _email) {
-        this._email = _email;
-    }
-
-    public String getTelefone() {
-        return _telefone;
-    }
-
-    public void setTelefone(String _telefone) {
-        this._telefone = _telefone;
-    }
-
     public Date getData_nascimento() {
-        return _data_nascimento;
+        return data_nascimento;
     }
 
-    public void setData_nascimento(Date _data_nascimento) {
-        this._data_nascimento = _data_nascimento;
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
     }
 
 }
