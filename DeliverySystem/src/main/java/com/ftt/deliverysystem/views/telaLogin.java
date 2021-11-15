2cf1265b5aebb7067f6d6af6baa6b1092684ab3d
cdArth <arthurjob@gmail.com>
2021-11-15 18:17:49 -0300

    Signed-off-by: cdArth <arthurjob@gmail.com>

--- DeliverySystem/src/main/java/com/ftt/deliverysystem/views/telaLogin.java
+++ DeliverySystem/src/main/java/com/ftt/deliverysystem/views/telaLogin.java
@@ -132,7 +132,6 @@ public class telaLogin extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(16, 16, 16)
                 .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
