0a6235c8ad703db4d322cd647e12073ec376a703
cdArth <arthurjob@gmail.com>
2021-11-15 15:10:02 -0300

    Signed-off-by: cdArth <arthurjob@gmail.com>

--- DeliverySystem/src/main/java/com/ftt/deliverysystem/controllers/ProdutoCategoriaController.java
+++ DeliverySystem/src/main/java/com/ftt/deliverysystem/controllers/ProdutoCategoriaController.java
@@ -4,10 +4,45 @@
  */
 package com.ftt.deliverysystem.controllers;
 
+import com.ftt.deliverysystem.dao.ClienteDAO;
+import com.ftt.deliverysystem.dao.EnderecoDAO;
+import com.ftt.deliverysystem.dao.ProdutoCategoriaDAO;
+import com.ftt.deliverysystem.dao.util.HelperDAO;
+import com.ftt.deliverysystem.models.ClienteModel;
+import com.ftt.deliverysystem.models.ProdutoCategoriaModel;
+import java.util.ArrayList;
+
 /**
  *
  * @author Arthur
  */
 public class ProdutoCategoriaController {
-    
+    public void Insert(ProdutoCategoriaModel model) {
+        HelperDAO hdao = new HelperDAO();
+        ProdutoCategoriaDAO dao = new ProdutoCategoriaDAO();
+        if (hdao.checkProduto(model)) {
+            dao.insert(model);
+        }
+    }
+
+    public void Update(ClienteModel cliente) {
+
+        EnderecoDAO dao = new EnderecoDAO();
+        cliente.setId(dao.find(cliente.getCpf()).getId());
+        dao.update(cliente);
+    }
+
+    public void Delete(ClienteModel cliente) {
+        EnderecoDAO dao = new EnderecoDAO();
+        dao.delete(cliente);
+
+    }
+
+    public ArrayList<ClienteModel> Listar() {
+        ClienteDAO dao = new ClienteDAO();
+        ArrayList<ClienteModel> lista = dao.findAll();
+        return lista;
+
+    }
+
 }
--- DeliverySystem/src/main/java/com/ftt/deliverysystem/dao/util/HelperDAO.java
+++ DeliverySystem/src/main/java/com/ftt/deliverysystem/dao/util/HelperDAO.java
@@ -7,25 +7,31 @@ package com.ftt.deliverysystem.dao.util;
 
         
 import com.ftt.deliverysystem.models.ClienteModel;
-import com.ftt.deliverysystem.models.EnderecoModel;
+
 import com.ftt.deliverysystem.models.UserModel;
 import com.ftt.deliverysystem.dao.EnderecoDAO;
 import com.ftt.deliverysystem.dao.UserDAO;
 import com.ftt.deliverysystem.dao.ClienteDAO;
+import com.ftt.deliverysystem.models.ProdutoCategoriaModel;
+
 /**
  *
  * @author Arthur
  */
 public class HelperDAO {
-    public boolean check(UserModel user,EnderecoModel adress, ClienteModel cliente){
+    public boolean checkUC(UserModel user, ClienteModel cliente){
         boolean check=false;
         UserDAO daoUser = new UserDAO();
         EnderecoDAO daoEndereco = new EnderecoDAO();
         ClienteDAO daoCliente = new ClienteDAO();
-        if(daoUser.find(user.getEmail())==null && daoEndereco.find(adress.getEndereco())==null 
+        if(daoUser.find(user.getEmail())==null
                 && daoCliente.find(cliente.getCpf())==null){
             check=true;
         }
+    return check;
+    }
+    public boolean checkProduto(ProdutoCategoriaModel produto){
+        boolean check=false;
     return check;
     }
 }
